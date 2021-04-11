8e9ff3a19aab33721a6c9c145fa521b7a49d8ea3
Jón Guðjónsson <jongudjonsson@gmail.com>
2021-04-11 15:03:48 +0000

    UI grunnur kominn

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("HotelSearchDemo.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 680, 520));
         primaryStage.show();
     }
 
